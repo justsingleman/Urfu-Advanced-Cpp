0a12f7ab584dada6c1012777ed13449ce2147dc5
justsingleman <164837790+justsingleman@users.noreply.github.com>
2025-01-09 21:00:35 +0300

    figures.hpp

--- task2/figures.hpp
+++ task2/figures.hpp
@@ -1,54 +1,66 @@
+#pragma once
+
 #include <memory>
 #include <stdexcept>
-constexpr double PI = 3.14;
-enum class FigureType
-{
+#include <string>
+
+enum class FigureType {
+    TRIANGLE,
+    CIRCLE,
+    RECTANGLE,
+};
+
 class Figure {
 public:
-    virtual ~Figure() = default;  // Add virtual destructor for proper cleanup
     virtual FigureType Type() const = 0;
     virtual double Perimeter() const = 0;
     virtual double Area() const = 0;
+    virtual ~Figure() = default;
 };
-class Rect : public Figure
-{
-private:
-    double width;
-    double height;
+
+class Rect : public Figure {
 public:
-    Rect(double a, double b);
+    Rect(double length, double height);
     FigureType Type() const override;
     double Perimeter() const override;
     double Area() const override;
-};
-class Triangle : public Figure
-{
+
 private:
-    double a, b, c;
+    double length_;
+    double height_;
+};
+
+class WrongTriangle : public std::invalid_argument {
 public:
-    Triangle(double side_a, double side_b, double side_c);
+    WrongTriangle(const std::string& message);
+};
+
+class Triangle : public Figure {
+public:
+    Triangle(double a, double b, double c);
     FigureType Type() const override;
     double Perimeter() const override;
     double Area() const override;
-};
-class Circle : public Figure
-{
+
 private:
-    double radius;
+    double a_, b_, c_;
+};
+
+class Circle : public Figure {
 public:
-    Circle(double r);
+    Circle(double radius);
     FigureType Type() const override;
     double Perimeter() const override;
     double Area() const override;
+
+private:
+    double radius_;
 };
+
+class LessThanZeroParam : public std::invalid_argument {
+public:
+    LessThanZeroParam(const std::string& message);
+};
+
 std::unique_ptr<Figure> make_figure(FigureType type, double a, double b = 0, double c = 0);
-class WrongTriangle : public std::invalid_argument
-{
-public:
-    using std::invalid_argument::invalid_argument;
-};
-class LessThanZeroParam : public std::invalid_argument
-{
-public:
-    using std::invalid_argument::invalid_argument;
-};
+
