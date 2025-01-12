fbf4e26a368246413b1df95f92ced3f82855edaf
justsingleman <164837790+justsingleman@users.noreply.github.com>
2025-01-12 13:47:18 +0500

    circular_queue.cpp

--- task3/circular_queue.cpp
+++ task3/circular_queue.cpp
@@ -48,34 +48,3 @@ bool CircularQueue::Empty() const {
 bool CircularQueue::Full() const {
     return count == maxSize;
 }
-int main() {
-    CircularQueue queue(5);
-
-    // Добавляем элементы
-    queue.Push(1);
-    queue.Push(2);
-    queue.Push(3);
-    queue.Push(4);
-    queue.Push(5);
-
-    std::cout << "Front: " << queue.Front() << std::endl; 
-    std::cout << "Back: " << queue.Back() << std::endl;   
-
-    // Попробуем добавить еще один элемент (должно вернуть false)
-    if (!queue.Push(6)) {
-        std::cout << "Queue is full!" << std::endl; 
-    }
-
-    // Удаляем элементы
-    queue.Pop();
-    queue.Pop();
-
-    std::cout << "Front: " << queue.Front() << std::endl; 
-    std::cout << "Back: " << queue.Back() << std::endl;  
-
-    // Добавляем новый элемент
-    queue.Push(6); 
-    std::cout << "Back after adding 6: " << queue.Back() << std::endl; 
-
-    return 0;
-}
