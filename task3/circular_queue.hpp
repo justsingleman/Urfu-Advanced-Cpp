da195c2c384efa69bb30001dcfdaf091ef64a4cc
justsingleman <164837790+justsingleman@users.noreply.github.com>
2025-01-12 12:31:55 +0500

    circular_queue.hpp

--- task3/circular_queue.hpp
+++ task3/circular_queue.hpp
@@ -1,26 +1,27 @@
 #pragma once
-#include <cstddef>
+#ifndef CIRCULAR_QUEUE_HPP
+#define CIRCULAR_QUEUE_HPP
+
 #include <vector>
+#include <stdexcept>
+
 class CircularQueue {
-private:
-    std::vector<int> buffer;
-    size_t capacity;
-    size_t size;
-    size_t front;
-    size_t rear;
 public:
     CircularQueue(size_t size);
+
     bool Push(int value);
     bool Pop();
     int Front() const;
     int Back() const;
     bool Empty() const;
     bool Full() const;
-    CircularQueue(size_t size);
-    bool Push(int value);
-    bool Pop();
-    int Front() const;
-    int Back() const;
-    bool Empty() const;
-    bool Full() const;
+
+private:
+    std::vector<int> buffer;
+    size_t maxSize;
+    size_t frontIndex;
+    size_t backIndex;
+    size_t count; // текущее количество элементов в очереди
 };
+
+#endif // CIRCULAR_QUEUE_HPP
